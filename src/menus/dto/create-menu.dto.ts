6238fb08119afbeac582a6d157acb3a56ceeeee4
Mohammad Kevin <kvn4.200581@gmail.com>
2026-05-27 22:29:48 +0700

    yuhuuuuuuuuuuuuuuuuuu

--- src/menus/dto/create-menu.dto.ts
+++ src/menus/dto/create-menu.dto.ts
@@ -13,7 +13,7 @@ import { Type } from 'class-transformer'
 export class CreateMenuDto {
   @IsString()
   @IsNotEmpty()
-  categoryId!: string
+  categoryId: string
 
   @IsString()
   @IsNotEmpty()
